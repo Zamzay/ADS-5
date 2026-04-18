82155ca4b6affe97d55bb8a92d8c338dd84e0ccd
Zamzay <93517658+Zamzay@users.noreply.github.com>
2026-04-18 22:49:11 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,27 +3,26 @@
 #define INCLUDE_TSTACK_H_
 #include <iostream>
 
-template<typename T, int size>
+template<typename T, int kMaxSize>
 class TStack {
-private:
-T arr[size];
-int top;
-public:
-TStack(): top(-1) {} 
-int getSize() const { return top + 1; }
-    
-void push(T value) { 
-  if (top + 1 >= size) std::cout << "Out of size" << std::endl;
-  else arr[++top] = value;
-}
-T pop() {
-  if (top != -1) return arr[top--];
-  else return T();
-}
-T getValue() const {
-  if (top != -1) return arr[top];
-  else return T();
-}
+ private:
+  T arr[size];
+  int top;
+ public:
+  TStack(): top(-1) {}
+  int getSize() const { return top + 1; }
+  void push(T value) {
+    if (top + 1 >= size) std::cout << "Out of size" << std::endl;
+    else arr[++top] = value;
+  }
+  T pop() {
+    if (top != -1) return arr[top--];
+    else return T();
+  }
+  T getValue() const {
+    if (top != -1) return arr[top];
+    else return T();
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
